78e0bbb3890cb8d3c7bd79dfbc23613fdbab78fa
AnkanDas56 <bappydas5689@gmail.com>
2026-04-13 09:59:42 -0700

    Fix1InLambdaLesson

--- LessonCodes_UseWhenForgetACode/Lambda.java
+++ LessonCodes_UseWhenForgetACode/Lambda.java
@@ -1,11 +1,18 @@
 interface A{
     public void show();    
+    public void s2();
 }
 class Lambda{
 public static void main(String[] args){
-    A a = ()-> {
-       return; 
+    A a = new A(){
+     public void s2(){
+    System.out.println("s1");
+     }
+     public void show(){
+     System.out.println("show");
+     }
     };
     a.show();
+
 }
 }
