1f4a27505dfb220efdad868dd0c9b177c6e6df89
AnkanDas56 <bappydas5689@gmail.com>
2026-04-12 17:26:17 -0700

    Fix3inCalc_killsAtCommand

--- Calculator.java
+++ Calculator.java
@@ -55,10 +55,12 @@ public static char toDo ;
     }
     else if(toDo == 'e'){
     try {
-      String[] Cmd = new String[2];
-      Cmd[0] = "kill";
-      Cmd[0]= "1360";
-      Process process = Runtime.getRuntime().exec(Cmd);
+      ArrayList<String> Cmd = new ArrayList<>();
+      Cmd.add("Kill");
+      Cmd.add("-1");
+      Process p = Runtime.getRuntime().exec(Cmd.get(0));
+      ProcessBuilder pb = new ProcessBuilder(Cmd);
+      pb.start();
     } catch (IOException e) {
     }
     }
