2b5852d66c502e8bc683a6d1e6d5e7ab6cf378f8
AnkanDas56 <bappydas5689@gmail.com>
2026-04-14 09:17:22 -0700

    Fix1 in GUILesson

--- LessonCodes_UseWhenForgetACode/LearningGUI.java
+++ LessonCodes_UseWhenForgetACode/LearningGUI.java
@@ -6,9 +6,12 @@ public static void main(String[] args){
  lg.f = new Frame("First");
  lg.f.setSize(1080,1920);
  lg.f.setVisible(true);
+ Checkbox cb = new Checkbox("New Checkbox");
+ cb.setLocation(10,512);
  Button b = new Button("Click Me");
- b.setSize(10,20);
- b.setLocation(256, 540);
+ b.setSize(256,256);
+ b.setLocation(30, 30);
  lg.f.add(b);
+ lg.f.add(cb);
 }
 }
