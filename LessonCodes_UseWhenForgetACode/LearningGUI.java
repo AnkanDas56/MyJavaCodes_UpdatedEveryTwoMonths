49d5d8dee640cd2c4a7b929d611386449e8213a5
AnkanDas56 <bappydas5689@gmail.com>
2026-04-14 09:04:57 -0700

    Added GUILesson

---
+++ LessonCodes_UseWhenForgetACode/LearningGUI.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+public class LearningGUI {
+Frame f;
+public static void main(String[] args){
+ LearningGUI lg = new LearningGUI();
+ lg.f = new Frame("First");
+ lg.f.setSize(1080,1920);
+ lg.f.setVisible(true);
+ Button b = new Button("Click Me");
+ b.setSize(10,20);
+ b.setLocation(256, 540);
+ lg.f.add(b);
+}
+}
