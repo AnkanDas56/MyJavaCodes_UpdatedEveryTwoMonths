14495b874b381e499e2a4c634a3bcfb7ffcc5931
AnkanDas56 <bappydas5689@gmail.com>
2026-04-14 13:15:24 -0700

    Added a checkbox

--- LessonCodes_UseWhenForgetACode/LearningGUI.java
+++ LessonCodes_UseWhenForgetACode/LearningGUI.java
@@ -6,9 +6,11 @@ public static void main(String[] args){
  lg.f = new Frame("First");
  lg.f.setSize(1080,1920);
  lg.f.setVisible(true);
+ Checkbox cb = new Checkbox();
  Button b = new Button("Click Me");
- b.setSize((int) Math.round(0.567),1);
+ b.setSize(256,256);
  b.setLocation(256, 540);
  lg.f.add(b);
+ lg.f.add(cb);
 }
 }
