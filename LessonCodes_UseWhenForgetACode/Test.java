cb1d3bee69611a393e923e624319d51b3a9c459d
AnkanDas56 <bappydas5689@gmail.com>
2026-05-22 17:34:17 -0700

    Finally renaming complete

--- LessonCodes_UseWhenForgetACode/Test.java
+++
@@ -1,7 +0,0 @@
-package New;
-public class Test{
-    public static String A = "hello";
-    public static void TestMethod(){
-        System.out.println("TestMethod");
-    }
-}
--- LessonCodes_UseWhenForgetACode/testThree.java
+++
@@ -1,6 +0,0 @@
-package New;
-public class testThree{
-    public static void testThreeM(){
-        System.out.println("testThreeMethod");
-    }
-}
