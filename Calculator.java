066fe9e04ea85d75db1a7200ddae363e80bd53d5
AnkanDas56 <bappydas5689@gmail.com>
2026-05-22 17:30:54 -0700

    Updated main class after finishing fixes in Calc and added LoginSystem Project

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,24 @@
 import java.awt.event.*;
 import java.awt.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.*;
+
 public class Calculator{
-    public static double value , keep ;
-    public double  num1 , num2;
+    public double value=0 , keep=0 ;
     public char toDo ;
     public int CC=0 ;
     public int CC2=0;
-    public int CC3=0; 
-
+    public double CC3=0; 
+    public double CC4=0;
     public void binaryOperations(char op){
-        keep = value;
-        value = 0 ;
+        this.keep = this.value;
+        this.value = 0;
         this.toDo=op;
         this.CC3++;
+        this.CC2++;
+    }
+    public void decimalPoint(){
+        this.CC4++;
     }
     public void add (){
         this.binaryOperations('+');
@@ -31,38 +35,47 @@ public class Calculator{
     public void squareOf(){
         this.binaryOperations('˜');
     }
-    public static void clear(){
-        value = 0;
-        keep = 0;
+    public void exit(){
+        this.binaryOperations('e');
+    }
+    public void clear(){
+        this.value = 0;
+        this.keep = 0;
         System.out.println("Cleared and restarting the calculator.");
     }
     public void digit(double x){
+        if(this.CC4==0){
         this.value = (this.value*10)+x;
-        this.CC2++;
+        }else if(this.CC4>0){
+            double digitAfterPoint = Math.pow(10,this.CC4);
+            double numsAfterPoint = x/digitAfterPoint;
+            this.value = this.value+numsAfterPoint;
+            this.CC4++;
+        }
     }
 
     public void compute(){
-    if (toDo == '+'){
-        value = keep + value;
+    if (this.toDo == '+'){
+        this.value = this.keep + this.value;
     }
-    else if (toDo == '-'){
-        value = keep - value;
+    else if (this.toDo == '-'){
+        this.value = this.keep - this.value;
     }
-    else if (toDo == '*'){
-        value = keep * value; 
+    else if (this.toDo == '*'){
+        this.value = this.keep * this.value; 
     }
-    else if (toDo == '/'){
-        value = keep / value;
+    else if (this.toDo == '/'){
+        this.value = this.keep / this.value;
     }
-    else if( toDo == '˜'){
-        value = keep*keep;
+    else if( this.toDo == '˜'){
+        this.value = this.keep*this.keep;
     }
-    else if( toDo=='!'){
-        clear();
+    else if( this.toDo=='c'){
+        this.clear();
     }
     else if(toDo == 'e'){
         System.exit(0);
-    return;
+        return;
     }
 }
     public double display(){
@@ -70,80 +83,191 @@ public class Calculator{
         return value;
     }
     public void compute2(){
-        Calculator calc = new Calculator();
       
         Scanner sc = new Scanner(System.in);
-        num1 = sc.nextDouble();
-        double m1 = num1 ;
+        double m1 =sc.nextDouble();
 
         Scanner sc3 = new Scanner(System.in);
         char op0 = sc3.next().charAt(0);
 
         Scanner sc2 = new Scanner(System.in);  
-        num2 = sc2.nextDouble();
-        double m2 = (double) num2 ;
+        double m2 = sc2.nextDouble();
        
     
-        digit(m1);        
-        binaryOperations(op0); 
-        digit(m2);
-        calc.compute();
-        display(); 
+        this.digit(m1);        
+        this.binaryOperations(op0); 
+        this.digit(m2);
+        this.compute();
+        this.display(); 
         clear(); 
     }
+
+
     public void disclaimer(){
-    System.out.println("Use the calculator for unlimited times and to terminat it, type 'exit'  in the space to enter numbers to terminate the calculator");
+    System.out.println("Use the calculator for unlimited times and to terminat it, type 'e'");
      this.CC++;
     }
 
-public static void main(String[] args) {
-    Calculator calc = new Calculator();
-        try {
-        if(calc.CC==0){
-        calc.disclaimer();
-        calc.compute2();
+public void go() {
+     try {
+        if(this.CC==0){
+        this.disclaimer();
+        this.compute2();
         }
-        else if(calc.CC>=1){
-            calc.compute2();
+        else if(this.CC>=1){
+            this.compute2();
         }
         } catch (InputMismatchException e) {
             System.err.println("Exited SuccessFully");
+            System.exit(0);
             return;
         }
-        Calculator.main(args);
+        this.go();
    }
+
+   //the final line of code in the calculator class the main method
+   public static void main(String[] args) {
+    Calculator cf = new CalculatorFrame();
+    cf.go();
+       }
+   }
+
+
+class CalculatorFrame extends Calculator {
+        JTextArea jta = new JTextArea(3,50);
+     
+        public void go(){
+          //The components
+          JFrame f = new JFrame();
+          JButton b;                    
+          JPanel bottom = new JPanel();
+          this.jta.setEditable(true);
+          this.jta.setText("click on a button to get started."+"\n"+" press C to clear");
+
+          //The Listeners
+          CalcOpButtonListener cobl = new CalcOpButtonListener(this);
+          CalcNumButtonListener cnbl = new CalcNumButtonListener(this,cobl);
+
+          //the upcoming methods :-
+          GridLayout mgr = new GridLayout(4,5);
+          bottom.setLayout(mgr);
+          for(int i =9;i>=0;i--){
+            b= new JButton(Integer.toString(i));b.setActionCommand(Integer.toString(i));
+            b.addActionListener(cnbl);
+            bottom.add(b);
+          }
+          b= new JButton("C");b.setActionCommand("C");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("=");b.setActionCommand("=");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("+");b.setActionCommand("+");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("-");b.setActionCommand("-");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("x");b.setActionCommand("*");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("÷");b.setActionCommand("/");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton("e");b.setActionCommand("e");b.addActionListener(cobl);bottom.add(b);
+          b= new JButton(".");b.setActionCommand(".");b.addActionListener(cobl);bottom.add(b);
+          b=new JButton("Sq");b.setActionCommand("˜");b.addActionListener(cobl);bottom.add(b);
+         bottom.setSize(295,285);
+         bottom.setBackground(Color.BLACK);
+         f.setBackground(Color.DARK_GRAY);
+         f.add("South",bottom);
+         f.add(this.jta);
+         f.setSize(295, 450);
+         this.jta.setEditable(true);
+         Font font = new Font(Font.SANS_SERIF,20,15);
+         this.jta.setFont(font);
+         this.jta.setForeground(Color.green);
+         this.jta.setBackground(Color.BLACK);
+         f.setVisible(true);
+    }
+    @Override
+    public double display(){
+    this.jta.setText(null);
+    this.jta.setText(Double.toString(this.value));
+    return this.value;
+    }
+    @Override 
+    public void clear(){
+        this.value = 0;
+        this.keep =0;
+        this.CC4 = 0;
+        this.jta.setText(null);
+    }
 }
 
-class CalculatorFrame implements ActionListener{
-    Calculator c = new Calculator();
+//the Action Listener for all the number buttons in the CalculatorFrame
+class CalcNumButtonListener implements ActionListener{
+    CalculatorFrame c ;
+    CalcOpButtonListener cobl;
+    public CalcNumButtonListener(CalculatorFrame c, CalcOpButtonListener opl){
+    this.c = c;
+    this.cobl = opl;
+    }
     @Override
     public void actionPerformed(ActionEvent e){
       String s = e.getActionCommand();
-      if(c.CC2==0){
-      int i = Integer.parseInt(s);
-      c.digit((double)i);
+      Double i ;
+      if (this.cobl.alreadyOnOperation&&this.c.toDo=='-'){
+        i = Double.valueOf(s);
+       this.c.digit(i*-1);
+       this.c.display();
+      } else{
+         i = Double.valueOf(s);
+         this.c.digit(i);
+         this.c.display();
       }
-      else if(c.CC2>=1){
-        char ch = s.charAt(0);
-        c.binaryOperations(ch);
-      }else if(c.CC3>=1){
-        int i = Integer.parseInt(s);
-        c.digit(i);
-        c.compute();
-        c.display();
+}
+}
+class CalcOpButtonListener implements ActionListener{
+    CalculatorFrame c;
+    protected boolean alreadyOnOperation = false;
+    public CalcOpButtonListener (CalculatorFrame calc){
+    this.c = calc;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        char ch = e.getActionCommand().charAt(0);
+    if(ch=='+'){
+        this.c.add();
+        this.c.CC4 = 0;
+        alreadyOnOperation = true;
+      }else if(ch=='*'){
+       this.c.multiply();
+       this.c.CC4 =0;
+        alreadyOnOperation = true;
+      }else if(ch=='/'){
+       this.c.divide();
+       alreadyOnOperation = true;
+        this.c.CC4 =0;
+      }else if(ch=='˜'){
+        this.c.CC4 =0;
+        this.c.squareOf();
+        this.alreadyOnOperation = true;
+        
+      }else if(ch=='-'){
+      this.c.subtract();
+      this.c.CC4 =0;
+      }else if(ch=='.'){
+        this.c.decimalPoint();
       }
+      else if(ch=='e'){
+        this.alreadyOnOperation = false;
+        this.c.exit();
+        System.exit(0);
+      }
+      else if(ch=='='){
+        this.c.compute();
+        this.c.display();
+        this.c.toDo = 0;
+        this.alreadyOnOperation = false;
+        this.c.CC4 =0;
+      }
+      else if (ch=='C'){
+      this.c.CC4 =0;
+      this.c.clear();
+      this.alreadyOnOperation = false;
+      }
+      if(ch!='='){
+      this.c.jta.setText(null);
+      this.c.jta.setText(Character.toString(ch));
     }
-    public void go(){
-        CalculatorFrame cf = new CalculatorFrame();
-         Frame f = new Frame();
-         Button b;                    
-         Panel bottom = new Panel();
-         for(int i =0;i<=9;i++){
-            b= new Button(Integer.toString(i));
-            bottom.add("East",b);
-         }
-         b = new Button("=");
-         bottom.add(b);
-        f.add("South",bottom);
-    }
+}
 }
